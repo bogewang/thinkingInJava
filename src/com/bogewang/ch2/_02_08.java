e5b9c0dabd05b9e3f109acaea5ed5f14b927307d
bogewang <236598164@163.com>
2018-05-22 21:44:08 +0800

    1.转换编码

--- src/com/bogewang/ch2/_02_08.java
+++ src/com/bogewang/ch2/_02_08.java
@@ -14,4 +14,24 @@ public class _02_08 {
             System.out.println(person.num++);
         }
     }
+
+    public static void main(String[] args) {
+        System.out.println(m_1());
+    }
+    public static int m_1(){
+        int i = 10;
+        try{
+            System.out.println("start");
+            return i += 10;
+        }catch(Exception e){
+            System.out.println("error:"+e);
+        }finally{
+            if(i>10){
+                System.out.println(i);
+            }
+            System.out.println("finally");
+            return 50;
+        }
+    }
+
 }
