2fae4696ceaa5b47f53a6d2cc62e77e24ca100f1
bogewang <236598164@163.com>
2018-05-23 08:46:47 +0800

    1.类型转换

---
+++ src/com/bogewang/ch3/_15_01/CastingNumbers.java
@@ -0,0 +1,18 @@
+package com.bogewang.ch3._15_01;
+
+import org.junit.Test;
+
+/**
+ * 关于java的 的截取和舍入问题;
+ */
+public class CastingNumbers {
+
+    @Test
+    public void test(){
+        float fabove = -0.7f;
+        float fblew = -0.4f;
+
+        System.out.println("(int)fabove:" + (int)fabove);
+        System.out.println("(int)fblew:" + (int)fblew);
+    }
+}
---
+++ src/com/bogewang/ch3/_17_01/StringTest.java
@@ -0,0 +1,17 @@
+package com.bogewang.ch3._17_01;
+
+import org.junit.Test;
+
+public class StringTest {
+
+    @Test
+    public void test(){
+
+
+    }
+
+
+    public void stringTest(String str1, String str2){
+        //System.out.println(str1 & str2);
+    }
+}
